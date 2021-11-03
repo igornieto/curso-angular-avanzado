6f80ec163f703ed0f483a1e6eecb1393da203adf
Igor Nieto <igor@workoholics.es>
2021-11-03 11:44:14 +0100

    [wip]

--- my-first-app/src/app/movies/containers/movies-page.component.ts
+++ my-first-app/src/app/movies/containers/movies-page.component.ts
@@ -8,16 +8,28 @@ import { Movie } from "../models/movie.model";
       <header>
         <h1>Últimas películas</h1>
       </header>
-      <movie [movie]="movie"></movie>
+      <div class="list-movies">
+        <movie *ngFor="let movie of movies" [movie]="movie"></movie>
+      </div>
     </div>
   `
 })
 export class MoviesPageComponent {
-  movie: Movie = {
-    title: 'Venom: Let There Be Carnage',
-    overview: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    adult: true,
-    vote: 6.9,
-    poster: 'https://image.tmdb.org/t/p/w500/eENEf62tMXbhyVvdcXlnQz2wcuT.jpg'
-  }
+
+  movies: Movie[] = [
+    {
+      title: 'Venom: Let There Be Carnage',
+      overview: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      adult: true,
+      vote: 6.9,
+      poster: 'https://image.tmdb.org/t/p/w500/eENEf62tMXbhyVvdcXlnQz2wcuT.jpg'
+    },
+    {
+      title: 'Dune',
+      overview: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      adult: true,
+      vote: 8,
+      poster: 'https://image.tmdb.org/t/p/w500/d5NXSklXo0qyIYkgV94XAgMIckC.jpg'
+    }
+  ]
 }
